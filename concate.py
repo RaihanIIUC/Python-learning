bd96d2fd22dbc002d102ada736a3e07dd4c5e29c
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-02-11 08:39:25 +0600

    concate

---
+++ concate.py
@@ -0,0 +1,6 @@
+first_name = 'Md'
+lastName = "Raihan"
+
+
+print(first_name+" "+ lastName);
+print(f'My fullName: {lastName} {first_name}')
---
+++ inputandPrint.py
@@ -0,0 +1,2 @@
+yourName = input("hey friends please type your name?")
+print('My friends name is',yourName)
