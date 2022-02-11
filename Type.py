e6fb427e6a1ac522909dab1941edbb67095c4ffd
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-02-11 08:50:45 +0600

    playing with type

---
+++ Type.py
@@ -0,0 +1,10 @@
+isCoupone = True
+
+orderedProduct = 5
+
+user_name = 'Korim shikh'
+
+
+print(f'hello {user_name} Hey my friend , Your coupone is {isCoupone}, your ordered product is {orderedProduct}')
+
+print(f' your name type is {type(user_name)} , your coupone type is {type(isCoupone)} and your ordered product type is {type(orderedProduct)}')
