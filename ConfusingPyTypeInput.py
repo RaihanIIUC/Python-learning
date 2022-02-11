0ea3b1d4c37cf3ebdc026adcdf87f3ea5ee75d91
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-02-11 09:05:08 +0600

    it is about handling type in input moment

---
+++ ConfusingPyTypeInput.py
@@ -0,0 +1,13 @@
+from mimetypes import init
+
+
+#CheckOut = float(input('please enter your checkout code '))
+
+#houseNum = int(input('Please put your home num here '))
+
+largeNum = int(input('enter larger num you can think '))
+minNum = int(input('enter your min num you can thing we will add up it for you'))
+
+addAll = largeNum + minNum
+
+print(f'We made all imagination in one place for you {addAll}')
